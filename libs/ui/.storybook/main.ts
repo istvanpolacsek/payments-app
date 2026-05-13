973ee3342e6d4947a9d4e5f3cbdd1cbfe3ae7e1c
istvan.polacsek <istvan.polacsek@uicentric.com>
2026-05-13 14:16:44 +0200

    chore(ui): storybook
    - code quality fixes

--- libs/ui/.storybook/main.ts
+++ libs/ui/.storybook/main.ts
@@ -36,7 +36,7 @@ const config: StorybookConfig = {
   },
 };
 
-function getAbsolutePath(value: string): any {
+function getAbsolutePath(value: string) {
   return dirname(fileURLToPath(import.meta.resolve(`${value}/package.json`)));
 }
 
