e1269473b68c8e3ef6481f1e780fe7681f794f19
istvan.polacsek <istvan.polacsek@uicentric.com>
2026-05-13 17:56:57 +0200

    chore(ui): payments table states
    - introduce payments table messages for error and loading states

--- libs/ui/src/components/index.ts
+++ libs/ui/src/components/index.ts
@@ -2,4 +2,7 @@ export { default as Button } from './button/Button';
 export { default as Dialog } from './dialog/Dialog';
 export { default as Input } from './input/Input';
 export { default as PaymentsTable } from './payments-table/PaymentsTable';
+export { default as PaymentsTableBase } from './payments-table/PaymentsTableBase';
+export { default as PaymentsTableError } from './payments-table/ErrorMessage';
+export { default as PaymentsTableLoading } from './payments-table/LoadingMessage';
 export { default as StatusBadge } from './status-badge/StatusBadge';
